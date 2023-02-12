0da6ddc70a2f59828be81cdf08d526c38f0e7d1b
左风 <595563214@qq.com>
2023-02-12 15:39:39 +0800

    fix: 更改目录顺序

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -61,9 +61,9 @@ export default {
           { text: '三分钟学会 React', link: '/pages/quickStart/react/' },
           { text: '三分钟学会 Next.js', link: '/pages/quickStart/next/' },
           { text: '三分钟学会 React Router ', link: '/pages/quickStart/reactRouter/' },
+          { text: '三分钟学会 zustand + immer', link: '/pages/quickStart/zustand/' },
           { text: '三分钟学会 Chakra-UI ', link: '/pages/quickStart/chakra/' },
           { text: '三分钟学会 tailwind Css ', link: '/pages/quickStart/tailwind/' },
-          { text: '三分钟学会 zustand + immer', link: '/pages/quickStart/zustand/' },
           { text: '三分钟学会 ClickToComponent', link: '/pages/quickStart/clickToComponent/' },
         ]
       }
